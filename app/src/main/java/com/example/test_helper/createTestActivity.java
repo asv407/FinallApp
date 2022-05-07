570529abeb39ad6bf8f7610fd4237565195a895c
asv407 <asv408090@gmail.com>
2022-05-07 21:19:57 +0400

    C

--- app/src/main/java/com/example/test_helper/createTestActivity.java
+++ app/src/main/java/com/example/test_helper/createTestActivity.java
@@ -2,7 +2,10 @@ package com.example.test_helper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class createTestActivity extends AppCompatActivity
 {
@@ -12,5 +15,14 @@ public class createTestActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_test);
+        final Button b = (Button)findViewById(R.id.next_Button);
+        b.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick (View v)
+            {
+                Intent intent = new Intent(createTestActivity.this, createQuestionActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
