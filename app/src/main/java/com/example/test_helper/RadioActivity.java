692ae44ea7453b89941470cbc70cdd391c6f3ccc
asv407 <asv408090@gmail.com>
2022-05-07 22:29:43 +0400

    h

--- app/src/main/java/com/example/test_helper/RadioActivity.java
+++ app/src/main/java/com/example/test_helper/RadioActivity.java
@@ -6,23 +6,45 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class RadioActivity extends AppCompatActivity
 {
     public RadioGroup radioGroup11;
+    public RadioButton radioButton1;
+    public RadioButton radioButton2;
+    public RadioButton radioButton3;
+    public RadioButton radioButton4;
+    public TextView textView;
+    public int i = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-        radioGroup11 = findViewById(R.id.radioGroup1);
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_radio);
+        super.onCreate(savedInstanceState);
+        radioGroup11 = findViewById(R.id.radioGroup1);
+        radioButton1= findViewById(R.id.radio1);
+        radioButton2 = findViewById(R.id.radio2);
+        radioButton3 = findViewById(R.id.radio3);
+        radioButton4 = findViewById(R.id.radio4);
+        textView = findViewById(R.id.textQuestion);
+
+
         final Button b = findViewById(R.id.nexts_Button);
         b.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick (View v)
             {
-               // radioGroup11.check(R.id.radio1);
+                i++;
+                String as = "Example of question \n # " + String.valueOf(i);
+                textView.setText(as);
+                radioGroup11.check(R.id.radio1);
+                radioButton1.setText("example # " + String.valueOf(i));
+                radioButton2.setText("example # " + String.valueOf(i + 1));
+                radioButton3.setText("example # " + String.valueOf(i + 2));
+                radioButton4.setText("example # " + String.valueOf(i + 3));
             }
         });
     }
